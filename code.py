f9a21e0f84a2bc1e0642e7def3a9dbf42bd707ec
Nicolás Pozas <pozas91@gmail.com>
2018-01-23 20:19:37 +0100

    2018/01/23

--- code.py
+++ code.py
@@ -1,2 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import re
+
+path = "corpus.txt"
+
+letters = {}
+words = {}
+
+with open(path, "r", encoding="utf8") as file:
+    
+    for line in file.readlines():
+        
+        filtered = re.sub('[^A-Za-z\u00C0-\u017F]+', ' ', line)
+        lineClean = re.sub('[\s]+', ' ', filtered)
+        
+        preWords = filter(lambda x: len(x) > 1, lineClean.split(' '))
+        
+        for word in preWords:
+            
+            word = word.strip().lower()
+            
+            if word in words:
+                words[word] += 1
+            else:
+                words[word] = 1
+            
+        
