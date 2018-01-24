57cbec6ba115f681e1695186c3ac6f40ae4e4cd8
Nicolás Pozas García <pozas91@gmail.com>
2018-01-24 14:17:22 +0100

    Corpus getting and encoding done!

--- code.py
+++ code.py
@@ -2,27 +2,64 @@
 
 import re
 
-path = "corpus.txt"
-
+path = "data/corpus.txt"
 letters = {}
 words = {}
+totalWords = 0
+totalLetters = 0
 
-with open(path, "r", encoding="utf8") as file:
+encodingDictionary = {
+    '1': ['a', 'á', 'b', 'c'],
+    '2': ['d', 'e', 'é', 'f'],
+    '3': ['g', 'h', 'i', 'í'],
+    '4': ['j', 'k', 'l'],
+    '5': ['m', 'n', 'ñ', 'o', 'ó'],
+    '6': ['p', 'q', 'r', 's'],
+    '7': ['t', 'u', 'ú', 'v'],
+    '8': ['w', 'x', 'y', 'z']
+}
+
+def encoding(word):
+  res = ''
     
-    for line in file.readlines():
-        
-        filtered = re.sub('[^A-Za-z\u00C0-\u017F]+', ' ', line)
-        lineClean = re.sub('[\s]+', ' ', filtered)
-        
-        preWords = filter(lambda x: len(x) > 1, lineClean.split(' '))
-        
-        for word in preWords:
+  for letter in list(word):
+    for key in encodingDictionary:
+      if letter in encodingDictionary.get(key):
+        res += key
+        break
             
-            word = word.strip().lower()
-            
-            if word in words:
-                words[word] += 1
-            else:
-                words[word] = 1
-            
+  return res
+
+def applyStatistics(items, total):
+  for item in items:
+    items[item][0] = items[item][0] / totalWords
+
+
+# Programm execution
+with open(path, "r", encoding="utf8") as file:
+
+  for line in file.readlines():
+      
+    line = re.sub('[^A-Za-z\u00C0-\u017F]+', ' ', line)
+    line = re.sub('[\s]+', ' ', line)
+          
+    for word in map(lambda x: x.strip().lower(), line.split(' ')):
+              
+      if word in words:
+        words[word][0] += 1
+      else:
+        words[word] = [1, encoding(word)]
+          
+      totalWords += 1
+          
+      for letter in list(word):
+          
+        if letter in letters:
+          letters[letter][0] += 1
+        else:
+          letters[letter] = [1, encoding(letter)]
+              
+        totalLetters += 1
         
+applyStatistics(words, totalWords)
+applyStatistics(letters, totalLetters)
